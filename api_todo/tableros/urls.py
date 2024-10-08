bc042ce3537907f03f3d523f5ea0179f47f6b324
Javi0108 <jgarcia@eturia.es>
2024-10-08 19:34:34 +0100

    Modificaciones para obtener un solo tablero por id

--- api_todo/tableros/urls.py
+++ api_todo/tableros/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from .views import TablerosViewSet, ListasViewSet, TareasViewSet
 
 router = DefaultRouter()
-router.register(r'tableros', TablerosViewSet)
+router.register(r'tableros', TablerosViewSet, basename='tableros')
 router.register(r'listas', ListasViewSet)
 router.register(r'tareas', TareasViewSet)
 
