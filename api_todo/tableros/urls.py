859a55c9ece679e63e936351f4da6359865aed35
Javi0108 <jgarcia@eturia.es>
2024-10-11 19:27:16 +0100

    Modificaciones de consulta de tableros

--- api_todo/tableros/urls.py
+++ api_todo/tableros/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import TablerosViewSet, ListasViewSet, TareasViewSet
+from .views import TablerosViewSet, TablerosByIdViewSet, ListasViewSet, TareasViewSet
 
 router = DefaultRouter()
 router.register(r'tableros', TablerosViewSet, basename='tableros')
@@ -9,4 +9,5 @@ router.register(r'tareas', TareasViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('tableros/<int:tablero_id>', TablerosByIdViewSet.as_view({'get': 'list'}), name='listas-por-tablero')
 ]
--- api_todo/tableros/views.py
+++ api_todo/tableros/views.py
@@ -1,14 +1,41 @@
 from rest_framework import viewsets
 from .models import Tableros, Listas, Tareas
 from .serializers import TablerosSerializer, ListasSerializer, TareasSerializer
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+
+
 
 class TablerosViewSet(viewsets.ModelViewSet):
     queryset = Tableros.objects.all()
     serializer_class = TablerosSerializer
 
+class TablerosByIdViewSet(viewsets.ModelViewSet):
+    serializer_class = TablerosSerializer
+
+    def list(self, request, tablero_id):
+        if not tablero_id:
+            raise NotFound("El parámetro 'tablero_id' es requerido.")
+        try:
+            id = Tableros.objects.filter(tablero__id=tablero_id)  # Filtra por tablero_id
+            serializer = self.serializer_class(id, many=True)
+            return Response(serializer.data)
+        except Tableros.DoesNotExist:
+            raise NotFound(f"No se encontró el tablero con id {tablero_id}")
+
 class ListasViewSet(viewsets.ModelViewSet):
     queryset = Listas.objects.all()
     serializer_class = ListasSerializer
+
+    def get_queryset(self):
+        tablero_id = self.request.query_params.get('tablero_id')  # Obtenemos el tablero_id de los query params
+        if tablero_id:
+            try:
+                return Listas.objects.filter(tablero__id=tablero_id)  # Filtramos las listas por el id del tablero
+            except Tableros.DoesNotExist:
+                raise NotFound(f"No se encontró un tablero con el id {tablero_id}")
+        else:
+            raise NotFound("El parámetro 'tablero_id' es requerido.")
 
 class TareasViewSet(viewsets.ModelViewSet):
     queryset = Tareas.objects.all()
